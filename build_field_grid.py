0e3fb0710093ab73769b3ef06141f95cffc18112
ved <vedshah.email@gmail.com>
2026-04-28 17:07:29 -0500

    new version of script to build the field grid

--- build_field_grid.py
+++ build_field_grid.py
@@ -10,36 +10,38 @@ from m4opt.fov import footprint
 from m4opt import skygrid
 
 from visualizations import plot_field_grid
+from constants import *
 
-FOV_length = 17826.13*u.arcsec
-FOV_width = 16924.67*u.arcsec
-FOV = FOV_width**2
-FOV_g = FOV/4
 
 vertices = skygrid.sinusoidal(FOV_g)
 target_coord = vertices
 
 region = RectangleSkyRegion(SkyCoord(0 * u.deg, 0 * u.deg), FOV_length, FOV_width, angle=90*u.deg)
-footprints = footprint(region, target_coord)
 
-ra_deg = vertices.ra.to(u.deg).value
-dec_deg = vertices.dec.to(u.deg).value
-df = pd.DataFrame({"ra_deg": ra_deg, "dec_deg": dec_deg})
+def main():
+    footprints = footprint(region, target_coord)
 
-sorted_dec = np.sort(np.unique(dec_deg))
+    ra_deg = vertices.ra.to(u.deg).value
+    dec_deg = vertices.dec.to(u.deg).value
+    df = pd.DataFrame({"ra_deg": ra_deg, "dec_deg": dec_deg})
 
-for row in df.itertuples():
+    sorted_dec = np.sort(np.unique(dec_deg))
 
-    row_ra, row_dec = row.ra_deg, row.dec_deg
-    dec_idx = np.where(sorted_dec == row_dec)[0][0]
+    for row in df.itertuples():
 
-    sorted_ras_at_dec = np.sort(df[df["dec_deg"] == row_dec]["ra_deg"])
-    ra_idx = np.where(sorted_ras_at_dec == row_ra)[0][0]
+        row_ra, row_dec = row.ra_deg, row.dec_deg
+        dec_idx = np.where(sorted_dec == row_dec)[0][0]
 
-    df.loc[row.Index, "Field Name"] = f"{ra_idx}_{dec_idx}"
+        sorted_ras_at_dec = np.sort(df[df["dec_deg"] == row_dec]["ra_deg"])
+        ra_idx = np.where(sorted_ras_at_dec == row_ra)[0][0]
 
-df.to_csv("LS4_field_grid.csv", index=False)
-print(df)
+        df.loc[row.Index, "Field Name"] = f"{ra_idx}_{dec_idx}"
+
+    df.to_csv("LS4_field_grid.csv", index=False)
+    print(df)
 
 
-plot_field_grid(vertices, [footprints[1000] ,footprints[1001], footprints[1004]])
+    plot_field_grid(vertices, [footprints[1000] ,footprints[1001], footprints[1004]])
+
+if __name__ == "__main__":
+    main()
